c37a8943b0d509c26203ac2395e4fc626ed8c94e
Rashidalabri0803 <rashid92759346@gmail.com>
2024-11-04 14:02:21 +0000

    part 1

--- rentals/urls.py
+++ rentals/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('properties/', views.property_list, name='property_list'),
+    path('tenants/', views.tenant_list, name='tenant_list'),
+    path('leases/', views.lease_list, name='lease_list'),
+    path('add_property/add/', views.add_property, name='add_property'),
 ]
--- rentals/views.py
+++ rentals/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-from .models import Property
+from .forms import PropertyForm, TenantForm, LeaseForm
+from .models import Lease, Property, Tenant
 
 
 def property_list(request):
     properties = Property.objects.all()
     return render(request, 'property_list.html', {'properties': properties})
+
+def tenant_list(request):
+    tenants = Tenant.objects.all()
+    return render(request, 'tenant_list.html', {'tenants': tenants})
+
+def lease_list(request):
+    leases = Lease.objects.all()
+    return render(request, 'lease_list.html', {'leases': leases})
+
+def add_property(request):
+    if request.method == 'POST':
+        form = PropertyForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('property_list')
+    else:
+        form = PropertyForm()
+    return render(request, 'add_property.html', {'form': form})
