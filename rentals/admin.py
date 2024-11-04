0ad13c8c9bfa8d0fed9e23d46e77e6981f4bedff
Rashidalabri0803 <rashid92759346@gmail.com>
2024-11-04 13:47:40 +0000

    admin

--- rentals/admin.py
+++ rentals/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Property, Tenant, Lease
+
+from .models import Lease, Property, Tenant
+
 
 @admin.register(Property)
 class PropertyAdmin(admin.ModelAdmin):
