a4f9cbf6996668fad7193e7894ce56028a37ffb5
Rashidalabri0803 <rashid92759346@gmail.com>
2024-11-07 10:15:20 +0000

    views

--- rentals/views.py
+++ rentals/views.py
@@ -1,15 +1,108 @@
 from django.shortcuts import get_object_or_404, redirect, render
-
+from .models import Property, Tenant, Lease, Invoice, PaymentHistory, Amenity, Contact, Document, MaintenanceRecord
 from .forms import (
-    ContactForm,
-    DocumentsForm,
-    InvoicesForm,
-    MaintenanceRecordForm,
-    NoteForm,
     PropertyForm,
+    TenantForm,
+    LeaseForm,
+    InvoiceForm,
+    PaymentHistoryForm,
+    AmenityForm,
+    ContactForm,
+    DocumentForm,
+    MaintenanceRecordForm,
 )
-from .models import Invoice, Lease, Payment, Property, Tenant
 
+def property_list(request):
+    properties = Property.objects.all()
+    return render(request, "property_list.html", {"properties": properties})
+
+def property_detail(request, property_id):
+    property_obj = get_object_or_404(Property, id=property_id)
+    return render(request, "property_detail.html", {"property": property_obj})
+
+def add_property(request):
+    if request.method == "POST":
+        form = PropertyForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("property_list")
+    else:
+        form = PropertyForm()
+    return render(request, "add_property.html", {"form": form})
+
+def tenant_list(request):
+    tenants = Tenant.objects.all()
+    return render(request, "tenant_list.html", {"tenants": tenants})
+
+def add_tenant(request):
+    if request.method == "POST":
+        form = TenantForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("tenant_list")
+    else:
+        form = TenantForm()
+    return render(request, "add_tenant.html", {"form": form})
+
+def lease_list(request):
+    leases = Lease.objects.all()
+    return render(request, "lease_list.html", {"leases": leases})
+
+def add_lease(request):
+    if request.method == "POST":
+        form = LeaseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("lease_list")
+    else:
+        form = LeaseForm()
+    return render(request, "add_lease.html", {"form": form})
+
+def invoice_list(request):
+    invoices = Invoice.objects.all()
+    return render(request, "invoice_list.html", {"invoices": invoices})
+
+def add_invoice(request):
+    if request.method == "POST":
+        form = InvoiceForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("invoice_list")
+    else:
+        form = InvoiceForm()
+    return render(request, "add_invoice.html", {"form": form})
+
+def payment_history_list(request):
+    payment_histories = PaymentHistory.objects.all()
+    return render(request, "payment_history_list.html", {"payment_histories": payment_histories})
+
+def add_payment_history(request):
+    if request.method == "POST":
+        form = PaymentHistoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("payment_history_list")
+    else:
+        form = PaymentHistoryForm()
+    return render(request, "add_payment_history.html", {"form": form})
+
+def amenity_list(request, property_id):
+    property_obj = get_object_or_404(Property, id=property_id)
+    amenities = property_obj.amenities.all()
+    return render(request, "amenity_list.html", {"amenities": amenities, "property": property_obj})
+
+def add_amenity(request, property_id):
+    property_obj = get_object_or_404(Property, id=property_id)
+    if request.method == "POST":
+        form = AmenityForm(request.POST)
+        if form.is_valid():
+            amenity = form.save(commit=False)
+            amenity.property = property_obj
+            amenity.save()
+            return redirect("amenity_list", property_id=property_id)
+    else:
+        form = AmenityForm()
+    return render(request, "add_amenity.html", {"form": form, "property": property_obj})
 
 def contact_list(request, tenant_id):
     tenant = get_object_or_404(Tenant, id=tenant_id)
@@ -18,99 +111,49 @@ def contact_list(request, tenant_id):
 
 def add_contact(request, tenant_id):
     tenant = get_object_or_404(Tenant, id=tenant_id)
-    form = ContactForm(request.POST or None)
-    if form.is_valid():
-        form.save(tenant=tenant)
-        return redirect("contact_list", tenant_id=tenant_id)
-    return render(request, "add_contact.html", {"form": form, "tenant": tenant})
-    
-def invoice_list(request):
-    invoices = Invoice.objects.all()
-    return render(request, 'invoice_list.html', {'invoices': invoices})
-
-def add_invoice(request):
-    if request.method == 'POST':
-        form = InvoicesForm(request.POST)
+    if request.method == "POST":
+        form = ContactForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('invoice_list')
+            contact = form.save(commit=False)
+            contact.tenant = tenant
+            contact.save()
+            return redirect("contact_list", tenant_id=tenant_id)
     else:
-        form = InvoicesForm()
-    return render(request, 'add_invoice.html', {'form': form})
+        form = ContactForm()
+    return render(request, "add_contact.html", {"form": form, "tenant": tenant})
 
 def document_list(request, property_id):
-    property = get_object_or_404(Property, pk=property_id)
-    documents = property.documents.all()
-    return render(request, 'document_list.html', {'documents': documents, 'property': property})
-
-def add_document(request):
-    if request.method == 'POST':
-        form = DocumentsForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('document_list')
-    else:
-        form = DocumentsForm()
-    return render(request, 'add_document.html', {'form': form})
-    
-def property_list(request):
-    properties = Property.objects.all()
-    return render(request, 'property_list.html', {'properties': properties})
-
-def note_list(request, property_id):
     property_obj = get_object_or_404(Property, id=property_id)
-    notes = property_obj.notes.all()
-    return render(request, 'note_list.html', {'property': property_obj, 'notes': notes})
+    documents = property_obj.documents.all()
+    return render(request, "document_list.html", {"property": property_obj, "documents": documents})
 
-def add_note(request, property_id):
+def add_document(request, property_id):
     property_obj = get_object_or_404(Property, id=property_id)
-    if request.method == 'POST':
-        form = NoteForm(request.POST)
+    if request.method == "POST":
+        form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
-            note = form.save(commit=False)
-            note.property = property_obj
-            note.save()
-            return redirect('note_list', property_id=property_id)
+            document = form.save(commit=False)
+            document.property = property_obj
+            document.save()
+            return redirect("document_list", property_id=property_id)
     else:
-        form = NoteForm()
-    return render(request, 'add_note.html', {'form': form, 'property': property_obj})
+        form = DocumentForm()
+    return render(request, "add_document.html", {"form": form, "property": property_obj})
 
-def maintenance_list(request, property_id):
+def maintenance_record_list(request, property_id):
     property_obj = get_object_or_404(Property, id=property_id)
     maintenance_records = property_obj.maintenance_records.all()
-    return render(request, 'maintenance_list.html', {'property': property_obj, 'maintenance_records': maintenance_records})
+    return render(request, "maintenance_record_list.html", {"property": property_obj, "maintenance_records": maintenance_records})
 
 def add_maintenance_record(request, property_id):
     property_obj = get_object_or_404(Property, id=property_id)
-    if request.method == 'POST':
+    if request.method == "POST":
         form = MaintenanceRecordForm(request.POST)
         if form.is_valid():
             maintenance_record = form.save(commit=False)
             maintenance_record.property = property_obj
             maintenance_record.save()
-            return redirect('maintenance_list', property_id=property_id)
+            return redirect("maintenance_record_list", property_id=property_id)
     else:
         form = MaintenanceRecordForm()
-    return render(request, 'add_maintenance_record.html', {'form': form, 'property': property_obj})
-
-def tenant_list(request):
-    tenants = Tenant.objects.all()
-    return render(request, 'tenant_list.html', {'tenants': tenants})
-
-def lease_list(request):
-    leases = Lease.objects.all()
-    return render(request, 'lease_list.html', {'leases': leases})
-
-def payment_list(request):
-    payments = Payment.objects.all()
-    return render(request, 'payment_list.html', {'payments': payments})
-
-def add_property(request):
-    if request.method == 'POST':
-        form = PropertyForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('property_list')
-    else:
-        form = PropertyForm()
-    return render(request, 'add_property.html', {'form': form})
+    return render(request, "add_maintenance_record.html", {"form": form, "property": property_obj})
